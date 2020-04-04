140df47e8ad749df8e652c3aed79024554de2a72
wafuwafu13 <mariobaske@i.softbank.jp>
2020-04-04 18:16:53 +0900

    WORKS画面完成

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="works">
-    works
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols=12 sm=12 md=6 lg=6 xl=6>
+        <v-card shaped color="#BEEFE6">
+          <v-card-text>
+            <v-layout justify-center>
+              <h1>portfolio</h1>
+           </v-layout>
+           <v-col class="pt-5" cols=12>
+             <v-img src="@/assets/portfolio.png" height=300></v-img>
+           </v-col>
+           <v-layout justify-center>
+              <a class="pt-5" href="https://github.com/wafuwafu13/portfolio" target="_blank">Github</a>
+           </v-layout>
+           <v-layout justify-center>
+              <a class="pt-5" href="https://github.com/wafuwafu13/portfolio" target="_blank">Qiita</a>
+           </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols=12 sm=12 md=6 lg=6 xl=6>
+        <v-card shaped color="#BEEFE6">
+          <v-card-text>
+            <v-layout justify-center>
+              <h1>日本の統計</h1>
+           </v-layout>
+           <v-col class="pt-5" cols=12>
+             <v-img src="@/assets/e-stat-native.png" height=300></v-img>
+           </v-col>
+           <v-layout justify-center>
+              <a class="pt-5" href="https://github.com/wafuwafu13/e-stat-native" target="_blank">Github</a>
+           </v-layout>
+           <v-layout justify-center>
+              <a class="pt-5" href="https://qiita.com/wafuwafu13/items/3c1ad74e8984d67e0a95" target="_blank">Qiita</a>
+           </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
