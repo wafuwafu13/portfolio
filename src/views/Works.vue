9199b60d4d10f79904ce5f39d3f69b0bcceeeb0f
wafuwafu13 <mariobaske@i.softbank.jp>
2020-04-05 12:13:05 +0900

    リンクを修正

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -4,7 +4,7 @@
       <v-col cols=12 sm=12 md=6 lg=6 xl=6>
         <v-card shaped color="#CDFFF6">
           <v-layout justify-center>
-            <v-card-title>ポートフォリオ</v-card-title>
+            <v-card-title>田川裕隆のポートフォリオサイト</v-card-title>
           </v-layout>
           <v-img src="@/assets/portfolio.png" height="305"></v-img>
           <v-card-subtitle>
@@ -15,7 +15,7 @@
               <a class="pt-5" href="https://github.com/wafuwafu13/portfolio" target="_blank">Github</a>
             </v-layout>
             <v-layout justify-center>
-              <a class="pt-5" href="https://github.com/wafuwafu13/portfolio" target="_blank">Qiita</a>
+              <a class="pt-5" href="https://qiita.com/wafuwafu13/items/e86fd839a5728a1d5d43" target="_blank">Vue初心者が2日でポートフォリオサイトを作る</a>
             </v-layout>
           </v-card-subtitle>
         </v-card>
@@ -34,7 +34,7 @@
               <a class="pt-5" href="https://github.com/wafuwafu13/e-stat-native" target="_blank">Github</a>
            </v-layout>
            <v-layout justify-center>
-              <a class="pt-5" href="https://qiita.com/wafuwafu13/items/3c1ad74e8984d67e0a95" target="_blank">Qiita</a>
+              <a class="pt-5" href="https://qiita.com/wafuwafu13/items/3c1ad74e8984d67e0a95" target="_blank">個人開発をしたので技術とマーケティングの観点からまとめる</a>
            </v-layout>
            </v-card-subtitle>
         </v-card>
